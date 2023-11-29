35e54a651fa62b4a47d653d9f4aa41ce41827811
xLillium <nicolas.motillon@gmail.com>
2023-11-29 19:58:20 +0100

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,21 +5,17 @@ public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
     String command = args[0];
     if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
-      //  String bencodedValue = args[1];
-      //  String decoded;
-      //  try {
-      //    decoded = decodeBencode(bencodedValue);
-      //  } catch(RuntimeException e) {
-      //    System.out.println(e.getMessage());
-      //    return;
-      //  }
-      //  System.out.println(gson.toJson(decoded));
-
+        String bencodedValue = args[1];
+        String decoded;
+        try {
+          decoded = decodeBencode(bencodedValue);
+        } catch(RuntimeException e) {
+          System.out.println(e.getMessage());
+          return;
+        }
+        System.out.println(gson.toJson(decoded));
     } else {
       System.out.println("Unknown command: " + command);
     }
